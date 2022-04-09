2179172c9c9f80ded4b2088c27935487bf85c54f
Emilio kariuki <emilio113kariuki@gmail.com>
2022-04-09 04:11:13 +0300

    changed port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
 const app = express();
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 app.use(express.json());
 
 mongoose.connect(
